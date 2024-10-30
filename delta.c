43a0919dd6154e5fe6692ec4a298fc0d6c1b6af2
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-10-29 22:02:26 -0300

    Update delta.c

--- delta.c
+++ delta.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 int root(int a, int b, int c){
- int delta;
- delta= b + (- 4*a*c);
- printf("%i",delta); 
+    
+ int delta, raiz, raiz1, raiz2;
+ 
+ delta= (b*b) + (- 4*a*c);
+ raiz= sqrt(delta);
+ if (raiz >= 0){
+  raiz1= (-b + sqrt(delta))/2*a;
+  raiz2= (-b - sqrt(delta))/2*a;
+ }
+ printf("As raízes são: %i e %i.",raiz1,raiz2); 
 }
 
 int main(){
 
 int a, b, c;
 
-printf("Digite os coeficientes: ");
+printf("Digite os coeficientes:\n");
 scanf("%i %i %i",&a, &b, &c);
 
 root(a, b, c);
